08b25ac604aa0d7d58f47389d4c84c1c466ae146
KaluginAleksey <mr.kamc@yandex.ru>
2022-07-01 14:15:19 +0300

    create Files MultiException

---
+++ MultiException/MultiException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace profit\MultiExceptions;
+
+class MultiException extends \Exception
+{
+    private array $exceptions = [];
+
+    public function add(\Exception $exception) {
+        $this->exceptions[] = $exception;
+    }
+
+    public function all()
+    {
+        return $this->exceptions;
+    }
+
+}
